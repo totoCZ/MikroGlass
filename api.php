1dff3d8da719bb28911948c377eff7250f996eb3
Tom Hetmer <tom@hetmer.net>
2013-08-30 09:18:13 +0200

    August 30th

--- api.php
+++ api.php
@@ -17,7 +17,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-error_reporting(0);
 header('Content-Type: application/json');
 
 function fail($why) {
@@ -29,73 +28,72 @@ function fail($why) {
 
 if (!is_readable("config.ini"))
 	fail("Nothing to do, my friend.");
-$config = parse_ini_file("config.ini");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$user     = $config['user'];
-	$password = $config['password'];
-	
-	$tools = array(
-		'ping' => '/ping count=4',
-		'trace' => '/tool traceroute use-dns=yes',
-		'route' => '/ip route print',
-        'exactroute' => '/ip r pr de where dst-address=',
-		'peers' => '/routing bgp peer print',
-		'status' => '/routing bgp peer print status'
-		);
-	
-	$type    = $_POST['type'];
-	$server  = $config['fqdn'][$_POST['id']];
-	$command = $tools[$type];
-	
-	error_log($type . ' ' . $command . ' ' . $cmd);
+$config		= parse_ini_file("config.ini");
+$user 		= $config['user'];
+$password	= $config['password'];
 
-	if ($command == "" || $server == "")
-		fail('wrong parameters');
+$tools = array(
+	'ping'			=> '/ping count=4',
+	'trace'			=> '/tool traceroute use-dns=yes',
+	'route'			=> '/ip route print',
+	'exactroute'	=> '/ip r pr de where dst-address=',
+	'peers'			=> '/routing bgp peer print',
+	'status'		=> '/routing bgp peer print status'
+	);
 
-	$cmd = $_POST['command'];
+$type 		= $_POST['type'];
+$argument 	= $_POST['command'];
+$server 	= $config['fqdn'][$_POST['id']];
+$tool 		= $tools[$type];
 
-	if ($type == 'ping' || $type == 'trace' || $type == 'exactroute') {
-		if ($cmd == "")
-			fail('empty parameter');
-		
-		if ($type == 'exactroute')
-			$space = '';
-		else
-			$space = ' ';
-
-		if ($type == 'ping' || $type == 'trace') {
-			$host = gethostbynamel($cmd);
-			if($host) {
-				$cmd = $host[0];
-			}
-		}
-
-		$command = $command . $space . escapeshellcmd($cmd);
-		
-	}
-
-	if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN' && $config['password'] == '') { // linux no password
-		$fp = popen('ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' . $user . '@' . $server . ' ' . $command, 'r');
-	} else { // putty link
-		$fp = popen($config['path'] . ' -ssh -l ' .  $user . ' -pw ' . $password . ' ' . $server . ' ' . $command, 'r');
-	}
-
-	$out = null;
-	while (!feof($fp)) {
-		$out .= fgets($fp);
-	}
-	fclose($fp);
-	
-	echo json_encode(array(
-		'command' => $command,
-		'result' => $out
-		));
-	
-} else {
-	unset($config["user"]);
-	unset($config["password"]);
-
-	echo json_encode($config);
+if ($tool == "" || $server == "") {
+	fail('wrong parameters');
 }
+
+error_log($type . ' ' . $tool . ' ' . $argument);
+$exec = $tool;
+
+if ($type == 'ping' || $type == 'trace' || $type == 'exactroute') {
+	if ($argument == "") {
+		fail('empty parameter');
+	}
+
+	if ($type == 'exactroute') {
+		$space = '';
+	}
+	else {
+		$space = ' ';
+	}
+
+	if ($type == 'ping' || $type == 'trace') {
+		$host = gethostbynamel($argument);
+		if($host) {
+			$argument = $host[0];
+		} else {
+			fail('bad ip');
+		}
+	}
+
+	$exec = $tool . $space . escapeshellcmd($argument);	
+}
+
+if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN' && $config['password'] == '') {
+	// linux no password
+	$fp = popen('ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' . $user . '@' . $server . ' ' . $exec, 'r');
+} else {
+	// putty link
+	$fp = popen($config['path'] . ' -ssh -l ' .  $user . ' -pw ' . $password . ' ' . $server . ' ' . $exec, 'r');
+}
+
+$out = null;
+while (!feof($fp)) {
+	$out .= fgets($fp);
+}
+fclose($fp);
+
+echo json_encode(array(
+	'command'	=> $exec,
+	'result'	=> $out
+	));	
 ?>
---
+++ index.php
@@ -0,0 +1,128 @@
+﻿<?php
+$c = parse_ini_file("config.ini");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MikroGlass Looking Glass for Mikrotik routers</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="//fonts.googleapis.com/css?family=Josefin+Sans|Droid+Sans&amp;subset=latin" rel="stylesheet">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0-rc2/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+	<link href="./app.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+	<nav class="navbar ug-navbar navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="./" class="navbar-brand">
+					µ<i class="icon-glass"></i>
+					<?php echo $c['version'] ?>
+				</a>
+			</div>
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<?php $i = 0; ?>
+					<?php foreach($c['fqdn'] as $server) { ?>
+					<li class="server server-<?php echo $i ?>"><a href="javascript:selectServer(<?php echo $i ?>)"><?php echo $server ?></a></li>
+					<?php $i++;?>
+					<?php } ?>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo $c['companyUrl'] ?>"><?php echo $c['name'] ?></a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<a href="https://github.com/TomHetmer/MikroGlass"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+
+	<div class="container">
+
+		<div class="jumbotron">
+			<h1>
+				MikroGlass LG
+			</h1>
+			<a href="<?php echo $c['companyUrl'] ?>">
+				<img src="<?php echo $c['logo'] ?>" class="pull-right" alt="Company logo">
+			</a>
+			<p class="lead">
+				You have connected to a looking glass server operated by <a href="<?php echo $c['companyUrl'] ?>"><?php echo $c['name'] ?></a>.
+			</p>
+			<?php echo $c['customText'] ?>
+		</div>
+
+		<noscript>
+			<div class="alert alert-block">
+				<h4>Warning!</h4>
+				<p>This tool does not function without JavaScript. Sorry.</p>
+			</div>
+		</noscript>
+
+		<div class="page-header">
+			<h2>
+				Step 1
+				<small class="warhead">Select your weapon.</small>
+			</h2>
+		</div>
+
+		<form id="form">
+			<div class="input-group col col-lg-12">
+				<div class="input-group-btn">
+					<button id="button" class="btn btn-lg btn-danger" tabindex="-1">
+						<span id="btnText">Ping</span>
+					</button>
+					<button type="button" class="btn btn-lg btn-danger dropdown-toggle" data-toggle="dropdown" tabindex="-1">
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu">
+						<li><a href="javascript:switchAction('ping')">Ping</a></li>
+						<li><a href="javascript:switchAction('trace')">Traceroute</a></li>
+						<li><a href="javascript:switchAction('exactroute')">BGP Route</a></li>
+					</ul>
+				</div>
+				<input class="form-control input-lg" id="fqdn" type="text" onclick="$(this).val('')" placeholder="hetmer.net" autofocus="autofocus" required="required">
+			</div>
+			<span class="help-block">
+				Quickcheck:
+				<a href="javascript:quick('route')">IP Routes</a>
+				<a href="javascript:quick('peers')">BGP Peers</a>
+				<a href="javascript:quick('status')">BGP Status</a>
+			</span>
+		</form>
+
+		<div class="page-header">
+			<h2>
+				Step 2
+				<small class="command"></small>
+			</h2>
+		</div>
+
+		<pre class="result">Awaiting instructions.</pre>
+
+		<footer>
+			<p>
+				Powered by <a href='//github.com/TomHetmer/MikroGlass'>MikroGlass</a>.
+			</p>
+			<p>
+				<a href='//ug.runtimeapp.com'>
+					<img src="./vendor/made_mfm.png" class="mfm pull-right" alt="Mikrotik logo">
+				</a>
+			</p>
+		</footer>
+
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0-rc2/js/bootstrap.min.js"></script>
+	<script src="./app.js"></script>
+	
+</body>
+</html>
