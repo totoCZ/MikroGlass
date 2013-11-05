64132057a4171b0f4c93a30b4032c2361345517a
Blake Covarrubias <blake@covarrubi.as>
2013-11-05 01:46:34 -0700

    Use sprintf() to format command string

--- api.php
+++ api.php
@@ -33,7 +33,7 @@ $tools = array(
 	'ping'          => '/ping count=4',
 	'trace'         => '/tool traceroute count=1 use-dns=yes',
 	'exactroute'    => '/ip r pr de where dst-address=',
-	'route' 				=> ['/ip r pr de where bgp ', ' in dst-address'],
+	'route' 				=> '/ip r pr de where bgp %s in dst-address',
 	'ipv4-route-info'    => '/ip route print',
 	'ipv6-route-info'    => '/ipv6 route print',
 	'bgp-peer'      => '/routing bgp peer print',
@@ -81,7 +81,7 @@ if ($type == 'ping' || $type == 'trace' || $type == 'exactroute' || $type == 'ro
 
 	// Generic BGP route lookup
 	if ($type == 'route') {
-		$exec = $tool[0] . escapeshellcmd($argument) . $tool[1];
+		$exec = sprintf($tool, escapeshellcmd($argument));
 	}else{
 		$exec = $tool . $space . escapeshellcmd($argument);
 	}
